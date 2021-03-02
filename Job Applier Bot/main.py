0a7f4a366a63aed0f6b06b349938c20cce76ab73
aaditya-patil-0018 <ampatil.6204@gmail.com>
2021-03-02 22:18:30 +0530

    Added the files

--- Job Applier Bot/main.py
+++ Job Applier Bot/main.py
@@ -5,16 +5,17 @@ import time
 from bs4 import BeautifulSoup as bs
 
 #This is information needed for working the code
-job = 'software developer'#str(input('What job you are looking for: '))
-location = 'nashik'#str(input('Please, Type in Location for which you are looking for : '))
-email = 'bot.of.python@gmail.com'
-password = 'Thisisforproject123'
+job = str(input('What job you are looking for: '))
+location = str(input('Please, Type in Location for which you are looking for : '))
+email = str(input('Type your email here : '))
+password = str(input('Type your indeed password : '))
 
 # This will start the browser
-driver = webdriver.Firefox()
+driver = webdriver.Firefox()	
 
 # This is for signing in
 driver.get('https://secure.indeed.com/account/login')
+
 
 e_mail = driver.find_element_by_id('login-email-input')
 e_mail.send_keys(email)	
@@ -23,9 +24,19 @@ passw.send_keys(password)
 driver.find_element_by_id('login-submit-button').click()
 
 time.sleep(2)
-driver.find_element_by_class_name('gnav-Logo-icon').click()
+try:
+	driver.find_element_by_class_name('gnav-Logo-icon').click()
+except Exception:
+	print('You maybe Trapped in Captcha. Just Type in Done!')
+	done = str(input('Type Beside this when you are done : '))
+	while done == None:
+		time.sleep()
+	driver.find_element_by_id('login-password-input')
+	passw.send_keys(password)
+	time.sleep(2)
+	driver.find_element_by_class_name('gnav-Logo-icon').click()
 
-time.sleep(5)
+time.sleep(2)
 search = driver.find_element_by_id('text-input-what')
 search.send_keys(job)
 search2 = driver.find_element_by_id('text-input-where')
@@ -38,4 +49,15 @@ job_list = driver.find_elements_by_xpath("//div[@data-tn-component='organicJob']
 for each_job in job_list:
     job_title = each_job.find_elements_by_xpath(".//h2[@class='title']/a")[0]
     job_title.click()
+    time.sleep(4)
+    job_apply = each_job.find_elements_by_id("apply-button-container")
+    try:
+    	a = driver.find_element_by_class_name("indeed-apply-button").click()
+    	time.sleep(10)
+    	b = driver.find_element_by_id('form-action-continue').click()
+    	print('Done')
+    	driver.back()
+    	time.sleep(1)
+    except Exception:
+    	print('This is not onsite Apply Job!')
 driver.quit()
